fc801014eea0cb192951e1dc528a1dc8a5502d90
Pabba Sumanth <pabbas@uwindsor.ca>
2024-10-14 18:40:18 -0400

    User Creation Backend/DB

--- app.js
+++ app.js
@@ -3,20 +3,25 @@ require('dotenv').config();
 const cors = require('cors')
 const { PORT } = require('./config');
 const app = express();
-const {mongoose}=require('mongoose');
+const connection = require("./node-mongoDB/db.cjs");
+const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
 
 // Enable CORS for all routes
 app.use(cors());
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log('DB Connected'))
-.catch(()=>console.log('Db not Connected.'));
+app.use(express.json());
+connection();
 // Add an API route that returns some data
 app.get('/api/data', (req, res) => {
   res.json({ message: 'This is the data from the backend!' });
 });
 
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
 // Root route (optional)
 app.get('/', (req, res) => res.send('Hello World'));
 
 // Start the server
-app.listen(PORT, () => console.log('Server running on port 3100'));
+const port=process.env.PORT;
+app.listen(port, () => console.log('Server running on port 3100',{port}))
