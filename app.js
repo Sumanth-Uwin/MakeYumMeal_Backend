fed01df02583f9a585b88e67b280460c59f33786
Pabba Sumanth <pabbas@uwindsor.ca>
2024-11-02 18:28:36 -0400

    api-integration

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const connection = require("./node-mongoDB/db.cjs");
 const userRoutes = require("./routes/user");
 const authRoutes = require("./routes/auth");
+const recipeApi=require("./recipeApi");
 
 // Enable CORS for all routes
 app.use(cors());
@@ -15,6 +16,20 @@ connection();
 app.get('/api/data', (req, res) => {
   res.json({ message: 'This is the data from the backend!' });
 });
+app.get("/api/recipes/search", async (req, res) => {
+  const searchTerm = req.query.searchTerm
+  const page = parseInt(req.query.page);
+  const results = await recipeApi.searchRecipes(searchTerm, page);
+
+  return res.json(results);
+});
+
+app.get("/api/recipes/:recipeId/information", async (req, res) => {
+  const recipeId = req.params.recipeId;
+  const results = await recipeApi.getRecipeInstructions(recipeId);
+  return res.json(results);
+});
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
